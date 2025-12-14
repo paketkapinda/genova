cf0bc8954cf60177f70d1b40cc124c53950ad94b
paketkapinda <genovabuild@gmail.com>
2025-12-14 21:09:49 +0300

    Update index.ts

--- supabase/functions/pod-payment/index.ts
+++ supabase/functions/pod-payment/index.ts
@@ -6,7 +6,7 @@ serve(async (req) => {
 
   const supabase = createClient(
     Deno.env.get('SUPABASE_URL')!,
-    Deno.env.get('SUPABASE_SERVICE_ROLE_KEY')!
+    Deno.env.get('SERVICE_ROLE_KEY')!
   )
 
   // 1️⃣ Sipariş çek
@@ -42,3 +42,4 @@ serve(async (req) => {
 
   return new Response(JSON.stringify({ success: true }))
 })
+
